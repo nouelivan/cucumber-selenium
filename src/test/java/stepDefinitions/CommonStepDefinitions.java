6c7921a0a85f35875c51cef4ecbc3bba910c625f
Ivan Nouel <nouelij17@gmail.com>
2020-02-27 16:47:47 -0500

    Cucumber/Selenium

---
+++ src/test/java/stepDefinitions/CommonStepDefinitions.java
@@ -0,0 +1,20 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CommonStepDefinitions {
+
+	
+
+}
